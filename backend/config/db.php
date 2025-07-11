8a5a755884ab4e96e0bfcb84bbf7d6ba608a94b0
Enyinnaya-x <ekevictor84@gmail.com>
2025-07-11 08:04:55 +0100

    new db

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "db4free.net"; // db4free host
-$username = "bitbetuser"; // your db4free username
+$username = "bitbetuser2"; // your db4free username
 $password = "password"; // your db4free password
-$dbname = "bitbetdb"; // your db4free database name
+$dbname = "bitbetdb2"; // your db4free database name
 
 $conn = new mysqli($servername, $username, $password, $dbname, 3306); // port 3306 is required
 
