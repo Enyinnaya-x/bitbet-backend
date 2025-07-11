8553f676a87e5a46cae4af7b2493eaf37f392ef1
Enyinnaya-x <ekevictor84@gmail.com>
2025-07-11 09:28:22 +0100

    logout logic

---
+++ backend/auth/logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST");
+header('Content-Type: application/json');
+
+$response = ['success' => false];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Unset all session variables
+    $_SESSION = [];
+
+    // Destroy the session
+    session_destroy();
+
+    $response['success'] = true;
+}
+
+echo json_encode($response);
+?>
