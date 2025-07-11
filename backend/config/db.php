aab404fec248bfc486cde593a6be7f003a61c450
Enyinnaya-x <ekevictor84@gmail.com>
2025-07-11 07:42:25 +0100

    free db connect

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "localhost"; // Usually localhost
-$username = "root"; // Your MySQL username (default XAMPP is root)
-$password = ""; // Your MySQL password (default XAMPP is empty)
-$dbname = "bet_bit";
+$servername = "db4free.net"; // db4free host
+$username = "bitbetuser"; // your db4free username
+$password = "password"; // your db4free password
+$dbname = "bitbetdb"; // your db4free database name
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306); // port 3306 is required
 
 if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-};
-
+die("Connection failed: " . $conn->connect_error);
+}
 ?>
