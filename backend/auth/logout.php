be805e4db4a0edfa74c428f9acbff4eef196d0c1
Enyinnaya-x <ekevictor84@gmail.com>
2025-07-15 19:39:36 +0100

    omo mehn

--- backend/auth/logout.php
+++ backend/auth/logout.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+ob_start();
+
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST");
 header('Content-Type: application/json');
